35231a8e232f53f770dab60f37bccdc017fc7273
ebolo <daothanhduy305@gmail.com>
2017-07-07 23:19:30 +0700

    Util for updating to Elastic search db

---
+++ EboloCommons/src/ebolo/libma/commons/ui/utils/GenericUtils.java
@@ -0,0 +1,10 @@
+package ebolo.libma.commons.ui.utils;
+
+/**
+ * Created by Ebolo on 07-Jul-17.
+ */
+public class GenericUtils {
+    public static String util(String s) {
+        return s + ' ';
+    }
+}
